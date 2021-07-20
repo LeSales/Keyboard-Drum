28955124812d34cc74d8000d170151d56418f0ad
Leandro Sales <leandrossales.96@gmail.com>
2021-07-20 07:46:42 -0300

    Add composition script to play songs

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector(".composer button").addEventListener('click', ()=>{
+    let song = document.querySelector("#input").value;
+
+    if(song !="") {
+        let songArray = song.split("");
+        playComposition(songArray);
+    }
+});
+
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -17,5 +26,17 @@ function playSound(sound){
         setTimeout(()=>{
             keyElement.classList.remove("active");
         }, 300);
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+
+    for(let songItem of songArray){
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 250;
     }
 }
